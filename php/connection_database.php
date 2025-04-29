beabcfdd1809cb9c2f34689b398937d586070525
hamza <jamal2@yaho>
2025-04-29 10:53:09 +0100

    the last version of code

---
+++ php/connection_database.php
@@ -0,0 +1,55 @@
+<?php
+$servernamee = "localhost"; // or the IP address of your server
+$usernamee = "root"; // replace with your database username
+$passwordd = ""; // replace with your database password
+$databasee = "projet_web"; // replace with your database name
+
+// Create connection
+$conn = new mysqli($servernamee, $usernamee, $passwordd, $databasee);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: ");
+}
+echo "Connected successfully!" ;
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+   
+// 2. Récupérer les données du formulaire
+$nom = $_POST['username'];
+$email = $_POST['password'];
+
+// 3. Préparer et exécuter la requête SQL
+ $sql = "INSERT INTO etudiant(id_etudiant , nom,password) VALUES (1,'$nom', '$email')"; 
+ 
+
+
+// Requête pour chercher l'utilisateur
+//$sql = "SELECT * FROM  etudiant WHERE nom = '$nom' AND password = '$email'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "Connexion réussie ! Bienvenue $nom.";
+    header("location:../html/dashbord.html");
+} else {
+    echo "Nom d'utilisateur ou mot de passe incorrect.";
+}
+
+
+
+
+
+// end compar 
+// if ($conn->query($sql) === TRUE) {
+//     echo "Données insérées avec succès !";
+//     header("location: ../html/dashbord.html");
+// } else {
+//     echo "Erreur dans la exucution des donnee : " . $sql . "<br>" . $conn->error;
+// }
+
+// 4. Fermer la connexion
+$conn->close();
+}
+
+?>
+
---
+++ php/script_1.php
@@ -0,0 +1,18 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// $conn = new mysqli('localhost','root','','projet_web');
+// if ($conn->connect_error) {
+//     die("connection to databases failed").$conn ->connect_error;
+// }
+// $stmt = $conn -> prepare("insert into etudiant(email,password)
+//  values(?,?) ");
+//  $stmt->bind_param("ss", $username , $password);
+//  $stmt->execute();
+//  echo "seccufucly";
+//  $stmt->close();
+//  $conn->close();
+
+ ?>
